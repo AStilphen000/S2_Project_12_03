2437dc0f61d411f8216b636ad88dced22b484181
Angelina Stilphen <astilphen000@west-mec.org>
2019-03-06 17:17:25 -0700

    Final Final Commit
    Ready for review

--- ah_report.js
+++ ah_report.js
@@ -28,24 +28,34 @@
       
 */
 
+// created a variable for the donation total equal to the initail value of 0. 
 var donationTotal = 0;
 
+// added a variable to effect the donors for each. Method to the donors array, using the callback function calcSum(). This statement will calculate the donation total.
 donors.forEach(calcSum);
 
-var summaryTable = "<table> <tr><th>Donors</th><td>" + donors.length + "</td></tr> <tr><th>Total Donations</th><td>$" + donationTotal.toLocaleString() + "</td></tr></table>";
+// created a variable named summarytable storing the text below. Where donors is the length of the donors.array, and total is the value of the donationTotal variable, preceded by a $.Applied the toLocaleString() method to the donationTotal variable so that the total amount of donations is displayed with a thousands separator in the report 
+var summaryTable = "<table> <tr><th>Donors</th><td>" + donors.length + "</td></tr> <tr><th>Total Donations</th><td>$ " + donationTotal.toLocaleString() + "</td></tr> </table>";
 
-document.getElementById('donationSummary').innerHTML = summaryTable;
+//  Set the innerHTML property of the div element with the ID donationSummary to the value of the summaryTable variable.
+document.getElementById("donationSummary").innerHTML = summaryTable;
 
-var majorDonors = doors.filter(findMajorDonors);
+// Added to the report to show a list of the donors who contributed $1000 or more to Appalachian House. Using the filter() method with the callback function findMajorDonors(), then created an array named majorDonors.
+var majorDonors = donors.filter(findMajorDonors);
 
+// Added the major donors list sorted in descending order. Apply the sort() method to the majorDonors variable using the callback function donorSortDescending().
 majorDonors.sort(donorSortDescending);
 
+//  created a variable named donortable that will store the HTML code for the table of major donors. Then set the initial value of the variable to the text string below.
 var donorTable = "<table> <caption>Major Donors</caption> <tr><th>Donation</th><th>Donor ID</th> <th>Date</th><th>Name</th><th>Address</th> <th>Phone</th><th>E-mail</th></tr>";
 
+// Created the HTML code for each donor row by applying the forEach() method to the majorDonors variable, using writeDonorRow() as the callback function.
 majorDonors.forEach(writeDonorRow);
 
-donorTable = "<table/>";
+// Added the text string </table> to the value of the donorTable variable.
+donorTable += "<table/>";
 
+//  Set the innerHTML property of the div element with the ID donorTable to the value of the donorTable variable.
 document.getElementById("donorTable").innerHTML = donorTable; 
 
 
